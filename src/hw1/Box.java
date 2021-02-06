0ff2c0fbbfb9bc8728d669cd893f46a0e52ed172
Andrew <shpuntik0902@gmail.com>
2021-02-06 14:12:32 +0300

    hw1

---
+++ src/hw1/Box.java
@@ -0,0 +1,33 @@
+package hw1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Box<T extends Fruit> {
+    private ArrayList<T> fruitList;
+    private String fruitName;
+
+    public void addFruit(T fruit){
+        fruitList.add(fruit);
+        if (fruitList.isEmpty())
+            fruitName = fruit.getClass().getName();
+    }
+    
+    public double getWeight(){
+        int count = fruitList.size();
+        if (count==0) 
+            return 0;
+        return fruitList.get(0).getWeight()*count;
+    }
+
+    public boolean compare(Box box){
+        return this.getWeight()==box.getWeight();
+    }
+
+    public void pourOverTo(Box<T> destBox){
+        for (T fruit : fruitList){
+            destBox.addFruit(fruit);
+        }
+        fruitList.clear();
+    }
+}
