9e8314eb543328448177e3ec322ac4aa51430132
Andrew <shpuntik0902@gmail.com>
2021-02-16 19:29:22 +0300

    hw4

---
+++ src/hw3/client/ServerService.java
@@ -0,0 +1,21 @@
+package hw3.client;
+
+import hw3.server.Message;
+
+import java.io.IOException;
+
+public interface ServerService {
+
+    boolean isConnected();
+
+    void openConnection();
+
+    void closeConnection();
+
+    String authorization(String login, String password) throws IOException;
+
+    void sendMessage(String message);
+
+    Message readMessages();
+
+}
